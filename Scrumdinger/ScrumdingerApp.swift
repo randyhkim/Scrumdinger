97dad9de8fc7561343d8695587006a4d888cf944
Randy Kim <randy.h.kim@vanderbilt.edu>
2022-05-20 23:13:34 +0900

    Initial project setup

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Randy Kim on 5/20/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
